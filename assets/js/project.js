7b556dad3c7abacbf76f6cccab5b32e0c76a1d79
Yu-Jen Lin <yjlin5210@gmail.com>
2017-06-20 22:09:31 -0400

    prevent scrolling when modal shows up on mobile device

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,11 +1,16 @@
 function onProjectClick(url) {
-    $('html').addClass('is-clipped');
+    $('html, body').addClass('is-clipped');
+    $('html, body').addEventListener('touchmove', function(e) {
+        e.preventDefault();
+    }, false);
+    
     $("#project-modal").addClass("is-active");
     
     $("#project-modal-content").load(url);
 }
 
 function closeModal(id) {
-    $('html').removeClass('is-clipped');
+    $('html, body').removeClass('is-clipped');
+    $('html, body').removeEventListener('touchmove');
     $("#"+id).removeClass("is-active");
 }
