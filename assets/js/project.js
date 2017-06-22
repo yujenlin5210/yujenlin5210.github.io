43c5a44b2b02048c0c787b005089b499f53ae40b
Yu-Jen Lin <yjlin5210@gmail.com>
2017-06-22 10:57:02 -0400

    manipulate browser history

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,4 +1,26 @@
-function onProjectClick(url) {
+var current_id = "";
+
+function onProjectClick(url, id="project-modal") {
+    current_id = id;
+    showModal(url);
+    history.pushState({
+        action: 1,
+        url: url,
+        id: id
+    }, document.getElementsByTagName("title")[0].innerHTML, "#noback");
+}
+
+function onCloseClick() {
+    closeModal(current_id);
+    history.pushState({
+        action: 0,
+        id: current_id
+    }, document.getElementsByTagName("title")[0].innerHTML, "");
+    current_id = "";
+    
+}
+
+function showModal(url) {
     // window.scrollTo(0,1);
     $('html').addClass('is-clipped');
     // $('html, body').on('touchmove', function(e) {
@@ -16,3 +38,17 @@ function closeModal(id) {
     // $('html, body').off('touchmove');
     $("#"+id).removeClass("is-active");
 }
+
+$(document).ready(function () {
+    window.onpopstate = function(event) {
+        // alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
+        if (event.state === null) {
+            alert(current_id);
+        } else if (event.state.action === 0) {
+            closeModal(event.state.id);
+        } else if (event.state.action === 1) {
+            showModal(event.state.url);
+        }
+    };
+});
+
