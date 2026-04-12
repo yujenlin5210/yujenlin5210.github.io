65a83293415af4507cac674f47197f1cd379c327
yjlin5210 <yjlin5210@gmail.com>
2026-04-11 21:49:50 -0700

    Configure Astro for GitHub Pages and add deployment workflow.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yujenlin5210.github.io',
   integrations: [react(), mdx()],
 
   vite: {
