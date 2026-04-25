dc8d81d2f657fb2b1921e8c85c1c40b98094f4e1
yjlin5210 <yjlin5210@gmail.com>
2026-04-25 15:18:51 -0700

    chore: consolidate stickman rig lab posts into a single canonical entry

--- src/components/StickmanIdleLab.jsx
+++
@@ -1,5 +0,0 @@
-import StickmanRigLab from './StickmanRigLab';
-
-export default function StickmanIdleLab() {
-  return <StickmanRigLab initialPhaseId="walking" initialClipId="walk" />;
-}
--- src/components/StickmanPoseWorkbench.jsx
+++
@@ -1,5 +0,0 @@
-import StickmanRigLab from './StickmanRigLab';
-
-export default function StickmanPoseWorkbench() {
-  return <StickmanRigLab initialPhaseId="standing" initialClipId="standing" />;
-}
